e00a4b72b847268ec246e4221b6a02e3dfab494f
nbacci <nmbachvarova@ucdavis.edu>
2016-11-24 12:57:07 -0800

    current version

---
+++ assets/js/sliderScript.js
@@ -0,0 +1,22 @@
+var slideIndex = 1;
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("menu");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
+function init(){
+    var d=new Date();
+    slideIndex=d.getDay();
+    showDivs(slideIndex);
+}
